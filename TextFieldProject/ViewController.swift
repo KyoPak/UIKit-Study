b9f7da2c13efc9ca4451930daf53c436efb62716
HyosungP <hyosing92@gmail.com>
2022-06-18 11:09:50 +0900

    TextField 기본 Func 역할 파악

--- TextFieldProject/ViewController.swift
+++ TextFieldProject/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     
     @IBOutlet weak var textField: UITextField!
@@ -16,6 +16,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textField.delegate = self
+        
         setup()
     }
 
@@ -29,6 +32,51 @@ class ViewController: UIViewController {
         textField.returnKeyType = .next
     }
 
+    //  텍스트필드의 입력을 시작할 대 호출 (시작할지, 말지 여부 허락하는 것)
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+    
+    // 시점 - 텍스트필드의 입력이 시작된 시점
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print(#function)
+        print("유저가 텍스트필드의 입력을 시작하였다.")
+    }
+    
+    // x버튼 사용여부
+    // 조건문 삽입가능
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+    
+    // 텍스트필드 글자 내용이 (한글자 한글자) 입력되거나 지워질대 호출이 되고 허락여부
+    // ex) 문자열 입력되면 false처리도 가능
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print(string)
+        print(#function)
+        return true
+    }
+    
+    // 텍스트필드의 엔터키가 눌러지면 다음 동작을 허락할것인지 말것인지
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+    
+    // 텍스트필드의 입력이 끝날 때 호출 (끝날지 말지를 허락)
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+
+    // 텍스트필드의 입력이 실제 끝났을때 호출 (시점)
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print(#function)
+        print("유저가 텍스트필드의 입력을 끝냈다.")
+    }
+    
     @IBAction func doneButtonTapped(_ sender: UIButton) {
         
     }
