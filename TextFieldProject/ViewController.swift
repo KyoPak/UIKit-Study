a263e957b9cc24540d2fa65fdb8c6d69613bca41
HyosungP <hyosing92@gmail.com>
2022-06-18 13:20:24 +0900

    TextField 글자 제한 추가, 엔터키 동작,  화면 터치시 응답객체 resign 추가

--- TextFieldProject/ViewController.swift
+++ TextFieldProject/ViewController.swift
@@ -26,13 +26,23 @@ class ViewController: UIViewController, UITextFieldDelegate {
         view.backgroundColor = UIColor.gray
         
         textField.keyboardType = UIKeyboardType.emailAddress
-        textField.placeholder = "이메일 입력"
+        textField.placeholder = "텍스트 입력"
         textField.borderStyle = .roundedRect
         textField.clearButtonMode = .always
         textField.returnKeyType = .next
+        
+        // 응답객체로 설정, 텍스트필드가 첫번째 반응객체가 되는것, 자동으로 키보드 띄워짐.
+        textField.becomeFirstResponder()
     }
+    
+    // 화면의 탭을 감지하는 메서드
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        // textField.resignFirstResponder() // 위와 동일
+    }
+    
 
-    //  텍스트필드의 입력을 시작할 대 호출 (시작할지, 말지 여부 허락하는 것)
+    //  텍스트필드의 입력을 시작할 때 호출 (시작할지, 말지 여부 허락하는 것)
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         print(#function)
         return true
@@ -54,15 +64,36 @@ class ViewController: UIViewController, UITextFieldDelegate {
     // 텍스트필드 글자 내용이 (한글자 한글자) 입력되거나 지워질대 호출이 되고 허락여부
     // ex) 문자열 입력되면 false처리도 가능
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        print(string)
-        print(#function)
-        return true
+        
+        // 입력되고 있는 글자가 "숫자" 인경우 입력 허용 안함
+        if(Int(string) != nil) { // 숫자로 변환된다면 nil아닐 테니
+            return false
+        } else {
+            // 10글자이상 입력 막는 코드
+            guard let text = textField.text else { return true }
+            let newLength = text.count + string.count - range.length
+            return newLength <= 10
+        }
+        
+//        if (textField.text?.count)! + string.count > 10 {
+//            return false
+//        }else {
+//            return true
+//        }
+        
     }
     
     // 텍스트필드의 엔터키가 눌러지면 다음 동작을 허락할것인지 말것인지
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         print(#function)
-        return true
+        
+        if textField.text == "" {
+            textField.placeholder = "Type Something!"
+            return false
+        } else {
+            return true
+        }
+        
     }
     
     // 텍스트필드의 입력이 끝날 때 호출 (끝날지 말지를 허락)
@@ -75,9 +106,12 @@ class ViewController: UIViewController, UITextFieldDelegate {
     func textFieldDidEndEditing(_ textField: UITextField) {
         print(#function)
         print("유저가 텍스트필드의 입력을 끝냈다.")
+        textField.text = ""
     }
     
     @IBAction func doneButtonTapped(_ sender: UIButton) {
+        // 응답객체 사임시키는 것.
+        textField.resignFirstResponder()
         
     }
     
