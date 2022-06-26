03b4c21a6e6f4b932340220883cdf40822542fdb
HyosungP <hyosing92@gmail.com>
2022-06-26 21:47:14 +0900

    no message

--- LoginProject/ViewController.swift
+++ LoginProject/ViewController.swift
@@ -228,10 +228,7 @@ final class ViewController: UIViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-    
-    
 }
-
 
 extension ViewController: UITextFieldDelegate {
     
