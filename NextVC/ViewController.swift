e4a6669620aa35d45e33f7f05761e9b966b0461a
HyosungP <hyosing92@gmail.com>
2022-07-02 15:45:13 +0900

    코드로 뷰컨트롤 이동 구현

--- NextVC/ViewController.swift
+++ NextVC/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    
+    // 1) 코드로 뷰컨트롤 화면 이동
     @IBAction func codeNextButtonTapped(_ sender: UIButton) {
         let firstVC = FirstViewController()
         firstVC.someString = "코드로 화면 바꾸기"
