1e80bd4186c4495e52c942529ea7e9e9e91096d3
HyosungP <hyosing92@gmail.com>
2022-07-02 20:07:36 +0900

    코드로 스토리보드 객체를 생성해서, 화면 이동 구현

--- NextVC/ViewController.swift
+++ NextVC/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    // 1) 코드로 뷰컨트롤 화면 이동
+    // 1) 코드로 뷰컨트롤 화면 이동 (다음화면이 코드로만 구성되어있을 때 사용가능)
     @IBAction func codeNextButtonTapped(_ sender: UIButton) {
         let firstVC = FirstViewController()
         firstVC.someString = "코드로 화면 바꾸기"
@@ -22,7 +22,15 @@ class ViewController: UIViewController {
         present(firstVC, animated: true, completion: nil)
     }
     
-    
-    
+    // 2) 코드로 스토리보드 객체를 생성해서, 화면 이동
+    @IBAction func storyboardWithCodeButtonTapped(_ sender: UIButton) {
+        
+        guard let secondVC = storyboard?.instantiateViewController(withIdentifier: "secondVC") as? SecondViewController else {
+            return
+        }
+        secondVC.someString = "코드로 스토리보드 객체를 생성해서 화면 이동"
+        secondVC.modalPresentationStyle = .fullScreen
+        present(secondVC, animated: true, completion: nil)
+    }
 }
 
