c1fc5923142780bc656ba4f7a856cb02037c999b
HyosungP <hyosing92@gmail.com>
2022-07-04 21:44:56 +0900

    bmi에 따라 색변경하는 메서드, bmi에 따라 문구 변경되는 메서드 구현, 결과값 Segue로 전달

--- BMI/ViewController.swift
+++ BMI/ViewController.swift
@@ -61,14 +61,17 @@ class ViewController: UIViewController {
             if let secondVC = segue.destination as? SecondViewController {
                
                 // 계산된 결과값 Segue로 전달
-                secondVC.bmi = self.bmi
-                
+                secondVC.bmiNumber = self.bmi
+                secondVC.bmiColor = getBackgroundColor()
+                secondVC.adviceString = getBMIAdviceString()
             } else {return}
         }
         // 다음화면으로 가기전에 텍스트필드 비우기
         heightTextField.text = ""
         weightTextField.text = ""
     }
+    
+    
     
     // BMI계산 메서드
     func calculateBMI(height: String, weight: String) -> Double {
@@ -79,6 +82,43 @@ class ViewController: UIViewController {
         return bmi
     }
     
+    func getBackgroundColor() -> UIColor {
+        guard let bmi = bmi else { return UIColor.black }
+        switch bmi {
+        case ..<18.6:
+            return UIColor(displayP3Red: 22/255, green: 231/255, blue: 207/255, alpha: 1)
+        case 18.6..<23.0:
+            return UIColor(displayP3Red: 212/255, green: 251/255, blue: 121/255, alpha: 1)
+        case 23.0..<25.0:
+            return UIColor(displayP3Red: 218/255, green: 127/255, blue: 163/255, alpha: 1)
+        case 25.0..<30.0:
+            return UIColor(displayP3Red: 255/255, green: 150/255, blue: 141/255, alpha: 1)
+        case 30.0...:
+            return UIColor(displayP3Red: 255/255, green: 100/255, blue: 78/255, alpha: 1)
+        default:
+            return UIColor.black
+        }
+    }
+    
+    
+    // 문자열을 얻어내는 메서드
+    func getBMIAdviceString() -> String {
+        guard let bmi = bmi else { return "" }
+        switch bmi {
+        case ..<18.6:
+            return "저체중"
+        case 18.6..<23.0:
+            return "표준"
+        case 23.0..<25.0:
+            return "과체중"
+        case 25.0..<30.0:
+            return "중도비만"
+        case 30.0...:
+            return "고도비만"
+        default:
+            return ""
+        }
+    }
 }
 
 
